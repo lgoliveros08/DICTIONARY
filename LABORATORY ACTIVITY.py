7d031306f2787519e46f296012ff47d43a14f76d
lgoliveros08 <lgoliveros16@gmail.com>
2025-02-16 00:20:14 +0800

    Add files via upload

---
+++ LABORATORY ACTIVITY.py
@@ -0,0 +1,11 @@
+TERMINOLOGY = { 
+                                "AMPLITUDE" : " MAXIMUM DISPLACEMENT FROM EQUILIBRIUM",
+                                "BEARING" : "ANGLE MEASURED CLOCKWISE FROM NORTH",                                    "BENDING MOMENT": "FORCE TENDING TO BEND A BEAM",
+                                "COEFFICIENT OF FRICTION" : "RATIO OF FRICTION FORCE TO NORMAL FORCE",
+                                "COMPRESSION" : "FORCE THAT SQUEEZES OR COMPRESSES A MATERIAL",
+                                "DEFLECTION" : "AMOUNT OF DISPLACEMENT UNDER LOAD",
+                                "DENSITY" : "MASS PER UNIT VOLUME",
+                                "EFFICIENCY" : "RATIO OF OUTPUT POWER TO INPUT POWER",
+                                 }                                
+x = input( "HI I'M THE ENGINEERING? DICTIONARY! WHAT WORD DO YOU WANT TO KNOW THAT IS RELATED TO ENGINEERING?")
+print( TERMINOLOGY[x])
